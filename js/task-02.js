d3626b0600207c65f07a430a7e1dd2242952be0d
Andrii Melnyk <108075582+Andrijkok@users.noreply.github.com>
2022-12-11 19:17:50 +0200

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+ingredients.forEach((element) => {  
+  const newElementLi = document.createElement("li");
+  newElementLi.textContent = element;
+  newElementLi.classList.add("item");
+  console.log(newElementLi); 
+ 
+  list.append(newElementLi);
+   
+});
+
+console.log(list);
