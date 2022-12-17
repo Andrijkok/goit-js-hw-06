029a5bdbbc2445ebaf74cad48d4e3a76c33eca29
Andrii Melnyk <108075582+Andrijkok@users.noreply.github.com>
2022-12-17 20:56:05 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+
+const handleClick = () => {
+  const newColor = getRandomHexColor();
+  console.log(newColor);
+  body.style.backgroundColor = newColor;
+  colorName.textContent = newColor;
+};
+
+button.addEventListener("click", handleClick);
