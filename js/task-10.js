ba3a315b74414c86eff374def5f48912645e40b2
Andrii Melnyk <108075582+Andrijkok@users.noreply.github.com>
2022-12-18 20:16:16 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,22 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const randomColor = getRandomHexColor();
+
+const newCollection = document.querySelector("#boxes");
+
+const elements = [];
+
+for (let i = 0; i <= 5; i += 1) {}
+
+const divEl = document.createElement("div");
+divEl.style.width = "30px";
+divEl.style.height = "30px";
+divEl.style.backgroundColor = randomColor;
+
+newCollection.append(divEl);
+
+console.log(divEl);
+
+console.log(newCollection);
